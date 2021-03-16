fe8d0900a47b29444c09960b1cd05262ba6d4b8d
Guido Fabio <guidofabio732@gmail.com>
2021-03-16 20:39:38 -0300

    Renames

--- controllers/tipo_maquina.js
+++
@@ -1,9 +0,0 @@
-const Sequelize     = require('sequelize');
-const tipo_maquina       = require('../models').tipo_maquina;
-module.exports = {
- list(_, res) {
-     return tipo_maquina.findAll({})
-        .then(tipo_maquina => res.status(200).send(tipo_maquina))
-        .catch(error => res.status(400).send(error))
- },
-};
--- controllers/tipo_pieza.js
+++
@@ -1,9 +0,0 @@
-const Sequelize     = require('sequelize');
-const tipo_pieza       = require('../models').tipo_pieza;
-module.exports = {
- list(_, res) {
-     return tipo_pieza.findAll({})
-        .then(tipo_pieza => res.status(200).send(tipo_pieza))
-        .catch(error => res.status(400).send(error))
- },
-};

b2c5a10dde32534f6c74cb46db3462bde930b11a
Guido Fabio <guidofabio732@gmail.com>
2021-03-20 19:05:03 -0300

    Mini changes

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ module.exports = (app) => {
    const tipoPiezaRoute = require('./tipo_pieza');
    const maquinaPiezaRoute = require('./maquina_pieza');
 
-   //Uso los middlewares
+   // Uso los middlewares
    app.use('/api/tipo_maquina', tipoMaquinaRoute);
    app.use('/api/tipo_pieza', tipoPiezaRoute);
    app.use('/api/maquina_pieza', maquinaPiezaRoute);
    
-   //Routes
+   // Routes
    app.get('/api', (req, res) => res.status(200).send({
       message: 'Example project did not give you access to the api web services'
    }));

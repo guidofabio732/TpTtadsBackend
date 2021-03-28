dbeb472da1bdcd419711e426a688a72af560a589
Juan Rueda <juani.rueda@hotmail.com>
2021-03-27 22:40:46 -0300

    Terminado regitro y login de usuario c manejo de errores

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,17 +1,55 @@
 const Sequelize = require('sequelize');
-const usuario = require('../models').usuarios;
+const Usuario = require('../models').usuarios;
 
 module.exports = {
 
-    create(req, res) {
-        return usuario
-            .create({
+    async register(req, res) {
+        try {
+            const usuario = await Usuario.create({
                 nombre: req.body.nombre,
                 apellido: req.body.apellido,
                 nombre_usuario: req.body.nombre_usuario,
                 password: req.body.password,
-            })
-            .then(() => res.status(200).send('Usuario creado'))
-            .catch(err => res.status(400).send(err))
+            });
+            res.status(200).json({
+                status: "Succeeded",
+                message: "Usuario creado",
+                data: usuario.id
+            });
+        } catch (err) {
+            res.status(400).json({
+                status: "failed",
+                message: err,
+                data: null
+            });
+        }
+    },
+    async login(req, res) {
+        try {
+            const usuario = await Usuario.findOne({
+                where: {
+                    nombre_usuario: req.body.nombre_usuario,
+                    password: req.body.password
+                }
+            });
+            if (usuario === null) {
+                res.status(404).json({
+                    status: "Failed",
+                    message: "Usuario o contraseña incorrectos",
+                    data: null
+                })
+            }
+            res.status(200).json({
+                status: "Succeeded",
+                message: "Logueado correctamente",
+                data: usuario.id
+            });
+        } catch (err) {
+            res.status(400).json({
+                status: "failed",
+                message: err.message,
+                data: null
+            });
+        }
     }
 }
--- routes/usuario.js
+++ routes/usuario.js
@@ -2,9 +2,12 @@ const express = require('express');
 const usuarioController = require('../controllers/usuario');
 const router = express.Router();
 
-//list
+//getOne by usuario y pass (login)
+
+router.post('/login', usuarioController.login);
 
 //create
-router.post('/create', usuarioController.create);
+router.post('/register', usuarioController.register);
+
 
 module.exports = router;
